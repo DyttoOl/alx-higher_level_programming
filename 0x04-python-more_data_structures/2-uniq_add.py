b4bd7170262df79f638a1771da28b97325ccc49e
DyttoOl <65666591+DyttoOl@users.noreply.github.com>
2022-06-08 16:56:57 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    for element in set(my_list):
-        sum += element
-     return sum
+    add = 0
+    for i in set(my_list):
+        add += i
+    return add
