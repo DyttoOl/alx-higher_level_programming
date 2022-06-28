6aa5ee3b61c60f29bfd159858dd4373853569080
DyttoOl <65666591+DyttoOl@users.noreply.github.com>
2022-06-28 15:07:00 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-class LockedClass:
-    """a class with no class or object attribute and prevents user from dynamically creating new instance attributes except when the new instance is called "first_name" """
+class LockedClass():
+    """a class that prevents user from dynamically creating new instance attributes,
+    except when the new instance is called first_name"""
     __slots__ = ['first_name']
