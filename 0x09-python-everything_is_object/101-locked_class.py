82451e8dd9f018f43b68231026ec11173a2718ad
DyttoOl <65666591+DyttoOl@users.noreply.github.com>
2022-06-28 15:14:20 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,9 @@
 class LockedClass():
     """Avoid dynamically creating new instance attributes,
     except if the new instance attribute is called first_name"""
-    __slots__ = "first_name"
+   
+
+__slots__ = "first_name"
 
     def __init__(self, first_name=None):
         self.first_name = first_name
